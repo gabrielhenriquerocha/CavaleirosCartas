4c159c41e7ed563d39eeb60f23e1c3e22fd2824e
Gabriel Henrique <ghrp@cin.ufpe.br>
2019-11-20 20:08:35 -0200

    aaaaaaa

--- Ip/src/cartas/CartaCavaleiroEfeito.java
+++ Ip/src/cartas/CartaCavaleiroEfeito.java
@@ -3,6 +3,6 @@ package cartas;
 public class CartaCavaleiroEfeito extends CartaCavaleiro {
     public CartaCavaleiroEfeito(String nome, String desc, int atq, int dfs , String signo, String classificacao, String constelacao,int id){
         super(nome,desc,atq,dfs,signo,classificacao,constelacao,id);
-
+//aaa
     }
 }
